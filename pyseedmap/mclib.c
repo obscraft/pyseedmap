3de180bc8f9f491aaa4715dfd615de9d81ede406
obscraft <123219490+obscraft@users.noreply.github.com>
2023-01-21 15:50:34 +0900

    Add files via upload

---
+++ pyseedmap/mclib.c
@@ -0,0 +1,58 @@
+// check the biome at a block position
+
+#include "../cubiomes/rng.h"
+#include "../cubiomes/finders.h"
+#include "../cubiomes/generator.h"
+#include "../cubiomes/layers.h"
+#include "../cubiomes/util.h"
+#include "../cubiomes/noise.h"
+#include "../cubiomes/quadbase.h"
+
+#include "../cubiomes/biome_tree.c"
+#include "../cubiomes/finders.c"
+#include "../cubiomes/generator.c"
+#include "../cubiomes/layers.c"
+#include "../cubiomes/util.c"
+#include "../cubiomes/noise.c"
+#include "../cubiomes/quadbase.c"
+
+#include <stdio.h>
+
+int isvillage(long seed, int xpos, int ypos, int zpos)
+{
+    Generator g;
+    setupGenerator(&g, MC_1_18, 0);
+
+    // Apply the seed to the generator for the Overworld dimension.
+    applySeed(&g, DIM_OVERWORLD, seed);
+
+    // To get the biome at a single block position, we can use getBiomeAt().
+    int scale = 1; // scale=1: block coordinates, scale=4: biome coordinates
+    
+    int biomeID = getBiomeAt(&g, scale, xpos, ypos, zpos);
+    if (biomeID == meadow || biomeID == plains || biomeID == savanna || biomeID == taiga || biomeID == desert || biomeID == snowy_taiga || biomeID == sunflower_plains || biomeID == snowy_plains)
+    {
+	    return 1;
+    }
+    else
+    {
+	    return 0;
+    };
+};
+
+int isvillagecheck(long seed, int xpos, int zpos)
+{
+    Generator g;
+    setupGenerator(&g, MC_1_18, 0);
+    applySeed(&g, DIM_OVERWORLD, seed);
+	
+    if (isViableStructurePos(Village, &g, xpos, zpos, 0))
+    {
+            return 1;
+    }
+    else
+    {
+            return 0;
+    };
+};
+    
---
+++ pyseedmap/wrap.c
@@ -0,0 +1,62 @@
+// check the biome at a block position
+//uwu
+#include "mclib.c"
+#include <stdio.h>
+#include <Python.h>
+#include <string.h>
+
+extern int isvillage(long, int, int, int);
+extern int isvillagecheck(long, int, int);
+
+static PyObject* pyisvillage(PyObject* self, PyObject* args)
+{
+	long seed; 
+	int xpos, ypos, zpos;
+	int result;
+	
+	if (!PyArg_ParseTuple(args, "liii", &seed, &xpos, &ypos, &zpos))
+        	return NULL;
+	result = isvillage( seed, xpos, ypos, zpos);
+	
+    return Py_BuildValue("i", result);
+
+};
+
+static PyObject* pyisvillagecheck(PyObject* self, PyObject* args)
+{
+	long seed; 
+	int xpos, zpos;
+	int result;
+	
+	if (!PyArg_ParseTuple(args, "lii", &seed, &xpos, &zpos))
+        	return NULL;
+	result = isvillagecheck( seed, xpos, zpos);
+	
+    return Py_BuildValue("i", result);
+
+};
+
+//meth>methods 
+static PyMethodDef isvillagemethods[] = {
+    { "isvillage", pyisvillage, METH_VARARGS},
+    { "isvillagecheck", pyisvillagecheck, METH_VARARGS},
+    { NULL},
+};
+
+
+
+// Module Definition Shit
+static struct PyModuleDef mclib = {
+    PyModuleDef_HEAD_INIT,
+    "mclib",
+    "lib for mcbe",
+    -1,
+   isvillagemethods
+};
+
+PyMODINIT_FUNC PyInit_mclib(void)
+{
+    return PyModule_Create(&mclib);
+}
+
+
