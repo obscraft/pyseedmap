3298bbff99887bacb06c294a6dc14af21a8dd72c
obscraft <123219490+obscraft@users.noreply.github.com>
2023-01-21 11:37:15 +0900

    Update __init__.py

--- Pycubiomes/__init__.py
+++ Pycubiomes/__init__.py
@@ -1,4 +1,4 @@
 from .genconsts import Versions, Biomes, Structures
-from .overworld import biome_at_pos, biomes_in_area, get_spawn
+from .overworld import biome_at_pos
 from .map import PyuMap
 from .nether import nether_biomes_in_area, nether_biome_at_pos
