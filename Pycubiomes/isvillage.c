b8aee801ee5ed6b7a4604fe2ab4d6982185376c3
obscraft <123219490+obscraft@users.noreply.github.com>
2023-01-21 12:30:49 +0900

    Update and rename overworld.c to isvillage.c

--- Pycubiomes/isvillage.c
+++ Pycubiomes/isvillage.c
@@ -9,7 +9,7 @@
 
 #include <stdio.h>
 
-int cBiomeAtPos(int64_t seed, int xpos, int ypos, int zpos)
+int cisVillage(int64_t seed, int xpos, int ypos, int zpos)
 {
     Generator g;
     setupGenerator(&g, MC_1_18, 0);
