3339871e7021e2a04e4ac3b89b3b91342b0d1f0f
obscraft <123219490+obscraft@users.noreply.github.com>
2023-01-21 15:49:08 +0900

    Delete Pycubiomes directory

--- Pycubiomes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .genconsts import Versions, Biomes, Structures
-from .overworld import biome_at_pos
-from .map import PyuMap
-from .nether import nether_biomes_in_area, nether_biome_at_pos
--- Pycubiomes/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('overwold',
-                    sources = ['wrap.c','overworld.c'],
-                    #extra_objects = ['hello.o'],
-                    )
-
-setup(name = 'overworld', version = '1.0.0', ext_modules = [module1])
